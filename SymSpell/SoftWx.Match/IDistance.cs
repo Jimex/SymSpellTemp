ca42b7d93a782b439792016a68c2637f7aae1898
Jimex <alexleeandroid@gmail.com>
2025-09-07 23:58:07 +0800

    first Commit

---
+++ SymSpell/SoftWx.Match/IDistance.cs
@@ -0,0 +1,25 @@
+﻿// Copyright ©2015-2018 SoftWx, Inc.
+// Released under the MIT License the text of which appears at the end of this file.
+// <authors> Steve Hatchett
+
+namespace SoftWx.Match {
+    /// <summary>Types implementing the IDistance interface provide methods
+    /// for computing a relative distance between two strings.</summary>
+    public interface IDistance {
+        /// <summary>Return a measure of the distance between two strings.</summary>
+        /// <param name="string1">One of the strings to compare.</param>
+        /// <param name="string2">The other string to compare.</param>
+        /// <returns>0 if the strings are equivalent, otherwise a positive number whose
+        /// magnitude increases as difference between the strings increases.</returns>
+        double Distance(string string1, string string2);
+
+        /// <summary>Return a measure of the distance between two strings.</summary>
+        /// <param name="string1">One of the strings to compare.</param>
+        /// <param name="string2">The other string to compare.</param>
+        /// <param name="maxDistance">The maximum distance that is of interest.</param>
+        /// <returns>-1 if the distance is greater than the maxDistance, 0 if the strings
+        /// are equivalent, otherwise a positive number whose magnitude increases as
+        /// difference between the strings increases.</returns>
+        double Distance(string string1, string string2, double maxDistance);
+    }
+}
---
+++ SymSpell/SoftWx.Match/ISimilarity.cs
@@ -0,0 +1,22 @@
+﻿namespace SoftWx.Match {
+    /// <summary>Types implementing the ISimilarity interface provide methods
+    /// for computing a normalized value of similarity between two strings.</summary>
+    public interface ISimilarity {
+        /// <summary>Return a measure of the similarity between two strings.</summary>
+        /// <param name="string1">One of the strings to compare.</param>
+        /// <param name="string2">The other string to compare.</param>
+        /// <returns>The degree of similarity 0 to 1.0, where 0 represents a lack of any
+        /// noteable similarity, and 1 represents equivalent strings.</returns>
+        double Similarity(string string1, string string2);
+
+        /// <summary>Return a measure of the similarity between two strings.</summary>
+        /// <param name="string1">One of the strings to compare.</param>
+        /// <param name="string2">The other string to compare.</param>
+        /// <param name="minSimilarity">The minimum similarity that is of interest.</param>
+        /// <returns>The degree of similarity 0 to 1.0, where -1 represents a similarity
+        /// lower than minSimilarity, otherwise, a number between 0 and 1.0 where 0
+        /// represents a lack of any noteable similarity, and 1 represents equivalent
+        /// strings.</returns>
+        double Similarity(string string1, string string2, double minSimilarity);
+    }
+}
